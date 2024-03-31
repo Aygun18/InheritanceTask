2272077151f92d83f59f5ea9c7c739c9c8076791
Aygun <sahmuradova18@gmail.com>
2024-03-31 23:58:40 +0400

    Inheritance Task

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inheritance
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Tapşırıq:
+			//Student classınız olsun. int[] Grades propertysi olsun.
+			//Studentdən obyekt yaradıb grades-i set edəndə arrayin yalnız cüt ədədlərindən ibarət yeni bir array set olunsun.
+			Student student = new Student();
+
+            student.Grades=new int[] {34,36,44,48,45,67,56,78,76,23,26};
+			student.GradesStudent();
+
+			
+
+        }
+	}
+}
---
+++ Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+	internal class Student
+	{
+		public int[] Grades;
+
+		public void GradesStudent()
+		{
+			int count = 0;
+
+			foreach (int grade in Grades)
+			{
+				if (grade % 2 == 0)
+				{
+					count++;
+				}
+			}
+
+
+			int[] evenGrades = new int[count];
+			int count1 = 0;
+			foreach(int grade in Grades)
+			{
+				if(grade%2==0)
+				{
+					evenGrades[count1]=grade;
+					count1++;
+				}
+			}
+			Grades = evenGrades;
+			foreach (int grade in evenGrades)
+			{
+                Console.WriteLine(grade);
+            }
+		}
+	}
+}
